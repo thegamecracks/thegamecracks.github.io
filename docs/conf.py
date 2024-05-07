461e720a8217f091809c3186933ed03bc1767fc2
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-05-07 10:46:53 -0400

    feat: set html_title to thegamecracks

--- docs/conf.py
+++ docs/conf.py
@@ -22,5 +22,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".gitkeep"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_title = "thegamecracks"
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
